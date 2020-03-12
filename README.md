5b351a8ad9ebe1b4e3e3941c5f58571870869570
Jason Maurer <jason@maur.co>
2020-03-12 11:34:30 -0600

    Some cleanup

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
-# Web Auth II Guided Project
+# Using Sessions and Cookies
 
-Guided project for **Web Auth II** Module.
+Guided project for **Node Auth 2** Module.
 
 ## Prerequisites
 
-- [SQLite Studio](https://sqlitestudio.pl/index.rvt?act=download) installed.
+- [DB Browser for SQLite](https://sqlitebrowser.org) installed.
+- A rest client like [Insomnia](https://insomnia.rest/download/) or [Postman](https://www.getpostman.com/downloads/) installed.
 
 ## Project Setup
 
 - [ ] fork and clone this repository.
 - [ ] **CD into the folder** where you cloned **your fork**.
-- [ ] type `npm i` to download dependencies.
+- [ ] type `npm install` to download dependencies.
 - [ ] type `npm run server` to start the API.
 
 Please follow along as the instructor adds support for `sessions` and `cookies` to the API.
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,40 +1,50 @@
-const router = require('express').Router();
-const bcrypt = require('bcryptjs');
+const express = require("express")
+const bcrypt = require("bcryptjs")
+const Users = require("../users/users-model")
+const restrict = require("../middleware/restrict")
 
-const Users = require('../users/users-model.js');
+const router = express.Router()
 
-// for endpoints beginning with /api/auth
-router.post('/register', (req, res) => {
-  let user = req.body;
-  const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
-  user.password = hash;
+router.post("/register", async (req, res, next) => {
+	try {
+		const { username } = req.body
+		const user = await Users.findBy({ username }).first()
 
-  Users.add(user)
-    .then(saved => {
-      res.status(201).json(saved);
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
-});
+		if (user) {
+			return res.status(409).json({
+				message: "Username is already taken",
+			})
+		}
 
-router.post('/login', (req, res) => {
-  let { username, password } = req.body;
+		res.status(201).json(await Users.add(req.body))
+	} catch(err) {
+		next(err)
+	}
+})
 
-  Users.findBy({ username })
-    .first()
-    .then(user => {
-      if (user && bcrypt.compareSync(password, user.password)) {
-        res.status(200).json({
-          message: `Welcome ${user.username}!`,
-        });
-      } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
-      }
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
-});
+router.post("/login", async (req, res, next) => {
+	try {
+		const { username, password } = req.body
+		const user = await Users.findBy({ username }).first()
+		const passwordValid = await bcrypt.compare(password, user.password)
 
-module.exports = router;
+		if (!user || !passwordValid) {
+			return res.status(401).json({
+				message: "Invalid Credentials",
+			})
+		}
+
+		res.json({
+			message: `Welcome ${user.username}!`,
+		})
+	} catch(err) {
+		next(err)
+	}
+})
+
+router.get("/logout", restrict(), (req, res, next) => {
+	// destroy the session here
+	res.end()
+})
+
+module.exports = router
